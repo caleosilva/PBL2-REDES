d8964f214fa0e394c0077043b39d28cec1ee6241
Caleo Silva <caleosilva75@gmail.com>
2023-12-06 13:55:39 -0300

    feat: lampard clock finished

--- chat.py
+++ chat.py
@@ -6,22 +6,22 @@ import time
 from queue import Queue
 
 
-
+'''
+Variávis de controle do código.
+'''
 HOST_LISTEN = "127.0.0.1"
-
 data_users = [
     {"host": HOST_LISTEN, "port": 1111, "nome": "jose"},
     {"host": HOST_LISTEN, "port": 2222, "nome": "maria"},
     {"host": HOST_LISTEN, "port": 3333, "nome": "rebeca"}
 ]
-
 my_info = {'host': HOST_LISTEN, 'port': '', 'nome': ''}
+mi_redes = []
 
-mi_redes = [
-    
-]
 
-# Relógio lógico
+'''
+Objeto que implementa o relógio lógico de Lamport.
+'''
 class LamportClock:
     def __init__(self):
         self.value = 0
@@ -38,8 +38,11 @@ class LamportClock:
             return self.value
 
 
-# Ouvido
-def server(clock, message_queue):
+'''
+Função que roda em uma thread e é responsável por receber as mensagens e colocá-las em uma fila 
+a fim de serem processadas.
+'''
+def server(message_queue):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_adress = (my_info['host'], my_info['port'])
     server_socket.bind(server_adress)
@@ -47,65 +50,80 @@ def server(clock, message_queue):
     while True:
         data, client_address = server_socket.recvfrom(1024)
         dataObj = json.loads(data)
-        print('\n', dataObj)
-        print(f"Received: {dataObj['msg']} at Lamport time {clock.value}\n")
-
-        clock.update(dataObj['time'])
-
-
         message_queue.put(dataObj)
 
-def handle_request(message_queue):
+'''
+Função que roda em uma thread e é responsável por pegar as solicitações da fila e
+processá-las.
+'''
+def handle_request(message_queue, clock):
     while True:
         message = message_queue.get()
-        print(f"Processing: {message['msg']} at Lamport time {message['time']}")
+        # print(f"Processing: {message['msg']} at Lamport time {message['time']}")
+        print("handle_request: ", message, '\n\n')
 
         try:
-            if message['type'] == 'sync':
-                # list_messages = module.recv_message_list(message, client_address, list_sync)
-                pass
-            elif message['type'] == 'msg':
+            if message['type'] == 'msg':
+                print("Vou atualizar no handle_request['msg']. Valor atual: ", clock.value)
+
+                clock.update(message['time'])
                 module.handle_mensagem(message, mi_redes, my_info)
-            elif message['type'] == 'on-sync':
-                pass
+            elif message['type'] == 'sync_clock':
+                objIndentificador = {'type': 'sync_clock_response', 'time': clock.value, 'sender': my_info}
+                module.responde_message(objIndentificador, my_info, message['sender'])
+            elif message['type'] == 'sync_clock_response':
+                module.sync_clock(clock, message)
+
         except Exception as e:
             print("Erro ao lidar com o request:", e)
 
 
+'''
+Função que roda em uma thread e é responsável sincronizar o relógio com todos os usuários online.
+'''
+def send_clock_to_sync(clock):
+    objIndentificador = {'type': 'sync_clock', 'time': clock.value, 'sender': my_info}
+    module.send_message(objIndentificador, my_info, data_users)
 
-def sync_messages():
-    # Exibe as mensagens
-    module.show_messages(mi_redes, my_info)
-
-# Manda mensagens
+'''
+Função que roda em uma thread e é responsável por controlar as mensagens de um usuário, mandando 
+para os demais e adicionando em sua lista.
+'''
 def write_prepare_message(clock):
     while True:
         mensagem = input("")
 
         if (mensagem != ""):
 
-            lamport_time = clock.increment()
+            # Cria o objeto da mensagem
             id_message = module.generete_id()
-
-            objMsg = {'type': 'msg', 'time': lamport_time, 'id': id_message, 'msg': mensagem, 'sender': my_info}
-            
-            #{'time': , 'id': '', 'msg': '', 'sender': {}},
-
-            # ELE TÁ RECEBENDO TYPE E NÃO TA TRATANDO ANTES DE ADD NA LISTA, TENHO QUE ALTERAR ISSO !!!!!!!!!!!!!!
+            objMsg = {'type': 'msg', 'time': clock.value, 'id': id_message, 'msg': mensagem, 'sender': my_info}
 
             # Envia a mensagem para a lista de mensagens local
             module.handle_mensagem(objMsg, mi_redes, my_info)
 
             # Envie a mensagem para outros usuários
             module.send_message(objMsg, my_info, data_users)
+
+            print("Vou atualizar no write_prepare_message. Valor atual: ", clock.value)
+            # Atualiza o relógio
+            clock.increment()
             
-            # mandar a nova lista para todos os usuários online
-            # module.send_message_list(mi_redes, my_info, data_users)
 
-            # receber a lista de todos e sincronizar
-    
+def sync_messages():
+    # 1- Mandar minha lista para todos.
+    # 2- Receber a lista de todos
+    # 3- Unir as listas
+    pass
 
-# mudar a forma que o login está sendo feito
+
+
+
+
+
+'''
+Função responsável por identificar o usuário e realizar o "LOGIN".
+'''
 def main():
     print('-=-=-=-= BEM VINDO =-=-=-=-\n\n')
 
@@ -120,29 +138,33 @@ def main():
         my_info['port'] = int(port)
         my_info['nome'] = nome
 
-        module.clear_screen()
+        
         start()
+        module.show_messages(mi_redes, my_info)
+
     elif opc == '2':
         exit()
 
-
+'''
+Função responsável por instanciar os objetos e iniciar as threads.
+'''
 def start():
     clock = LamportClock()
     message_queue = Queue()
 
-    accept_thread = threading.Thread(target=sync_messages, args=())
-    accept_thread.start()
-
-    accept_thread = threading.Thread(target=server, args=(clock, message_queue))
-    accept_thread.start()
-
-    process_thread = threading.Thread(target=handle_request, args=(message_queue,))
-    process_thread.start()
+    sync_clock_thread = threading.Thread(target=send_clock_to_sync, args=(clock,))
+    sync_clock_thread.start()
 
 
 
-    accept_thread = threading.Thread(target=write_prepare_message, args=(clock,))
-    accept_thread.start()
+    server_thread = threading.Thread(target=server, args=(message_queue,))
+    server_thread.start()
+
+    handle_request_thread = threading.Thread(target=handle_request, args=(message_queue, clock))
+    handle_request_thread.start()
+
+    write_prepare_message_thread = threading.Thread(target=write_prepare_message, args=(clock,))
+    write_prepare_message_thread.start()
 
 
 if __name__ == "__main__":
--- module.py
+++ module.py
@@ -7,25 +7,12 @@ import uuid
 '''
 Gera um ID único para a mensagem.
 '''
-
-
 def generete_id():
     return str(uuid.uuid4())
-
-
-# def get_my_latest_index(list_messages):
-#     last_id = False
-
-#     if (len(list_messages) > 0):
-#         last_id = list_messages[-1]['index']
-#     return last_id
-
 
 '''
 Limpa a tela tanto no linux quanto no windows
 '''
-
-
 def clear_screen():
     # Limpar a tela no Linux e macOS
     if os.name == 'posix':
@@ -35,28 +22,13 @@ def clear_screen():
     elif os.name == 'nt':
         os.system('cls')
 
-
-'''
-Função responsável por retornar se um usuário precisa sincronizar ou não
-'''
-
-
-def check_need_sync(id_local, latest_id):
-    if (id_local == ''):
-        return True
-    elif (int(latest_id) > int(id_local)):
-        return True
-    else:
-        return False
-
-
-'''
-Retorna a chave pela qual uma lista de dicionario deve ser ordenada
-'''
-
-
-def sort_key(dic):
-    return list(dic.keys())[0]
+def responde_message(objMsg, my_info, info_user):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    if (my_info['port'] != info_user['port'] and my_info['nome'] != info_user['nome']):
+        try:
+            client_socket.sendto(json.dumps(objMsg).encode(), (info_user['host'], info_user['port']))
+        except Exception as e:
+            print(e)
 
 def send_message(objMsg, my_info, data_users):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -67,40 +39,8 @@ def send_message(objMsg, my_info, data_users):
             except Exception as e:
                 print(e)
 
-def send_message_list(message_list, my_info, data_users):
-    try:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        for user in data_users:
-            if my_info['port'] != user['port'] and my_info['nome'] != user['nome']:
-                size_list = len(message_list)
-
-                for objMsg in message_list:
-                    objFormatado = {'size': size_list, 'type': 'sync', 'body': objMsg}
-                    client_socket.sendto(json.dumps(objFormatado).encode(), (user['host'], user['port']))
-                    
-    except Exception as e:
-        print("Erro durante o envio:", e)
-    finally:
-        # Fecha o socket após o envio
-        client_socket.close()
-
-def recv_message_list(dataObj, client_address, list_sync):
-
-    try:
-        list_sync.append()
-
-        print("dataObj: ", dataObj)
-        print("client_address: ", client_address)   
-        print("\n\n")
-
-    except KeyboardInterrupt:
-        print("Servidor encerrado pelo usuário.")
-
 def show_messages(group_messages, my_info):
-    # module.clear_screen()
-
-    print(group_messages)
+    clear_screen()
 
     print('--------------------------------------------------')
     print('|                   MI - REDES                   |')
@@ -121,3 +61,51 @@ def handle_mensagem(objMsg, lista_mensagens, my_info):
 
     lista_mensagens.append(copia)
     show_messages(lista_mensagens, my_info)
+
+def send_clock_to_sync(clock, my_info, data_users):
+    objIndentificador = {'type': 'sync_clock', 'time': clock.value}
+    send_message(objIndentificador, my_info, data_users)
+
+def sync_clock(clock, info):
+    try:
+        clock_received = int(info['time'])
+        if (clock_received > clock.value):
+            clock.update(info['time'])
+    except Exception as e:
+        print(e)
+
+
+
+
+
+
+
+
+def send_message_list(message_list, my_info, data_users, id_list):
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        size_list = len(message_list)
+
+        for user in data_users:
+            if my_info['port'] != user['port'] and my_info['nome'] != user['nome']:
+                for objMsg in message_list:
+                    objFormatado = {'id_list': id_list, 'size': size_list, 'type': 'sync_list', 'body': objMsg}
+                    client_socket.sendto(json.dumps(objFormatado).encode(), (user['host'], user['port']))
+                    
+    except Exception as e:
+        print("Erro durante o envio:", e)
+    finally:
+        client_socket.close()
+
+def recv_message_list(dataObj, client_address, list_sync):
+    try:
+        
+        list_sync.append()
+
+        print("dataObj: ", dataObj)
+        print("client_address: ", client_address)   
+        print("\n\n")
+
+    except KeyboardInterrupt:
+        print("Servidor encerrado pelo usuário.")
+
